aa2ed288f7a068695e9c4331f8ed773900378028
GitHiroki <dassimen.1903@gmail.com>
2026-06-14 23:32:24 +0900

    initial commit 環境構築

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  items: [{ autogenerate: { directory: 'reference' } }],
-              },
-          ],
-      }),
-	],
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    items: [{ autogenerate: { directory: 'reference' } }],
+                },
+            ],
+            head: [
+                {
+                    tag: 'meta',
+                    attrs: { name: 'robots', content: 'noindex, nofollow' },
+                },
+            ],
+            components: {
+                Footer: './src/components/GiscusComments.astro',
+            },
+        }),
+    ],
 
-  adapter: cloudflare(),
+    //   adapter: cloudflare(),
 });
